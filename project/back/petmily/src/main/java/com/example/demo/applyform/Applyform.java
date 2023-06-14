fdc4ca280bc93354ad336acc5c196345ef58b8f4
LAPTOP-G87K7OJC\user <johnchoi06888@gmail.com>
2023-06-14 17:01:07 +0900

    applyform

---
+++ project/back/petmily/src/main/java/com/example/demo/applyform/Applyform.java
@@ -0,0 +1,49 @@
+package com.example.demo.applyform;
+
+
+import java.util.Date;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.SequenceGenerator;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Applyform {
+	@Id
+	@SequenceGenerator(name="seq_gen", sequenceName="seq_applyform", allocationSize=1)//시퀀스 생성. sequenceName:시퀀스 이름
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="seq_applyform")//값 자동생성설정
+	private int num;
+	
+	@ManyToOne
+	@JoinColumn(name="member", nullable=false)  //fk 설정
+	@OnDelete(action = OnDeleteAction.CASCADE)
+	private Member id;
+	private Date date;
+	private String agreement;
+	private String another;
+	private String reason;
+	private String feeding;
+	
+	@PrePersist
+	public void preprocess() {
+		date = new Date(); //현재 날짜 생성
+	}
+}
