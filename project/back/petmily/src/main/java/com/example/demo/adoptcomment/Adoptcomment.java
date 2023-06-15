1c26569378d5a60cf98f24fb1ecc7322fe1ffe3a
sgyoung96 <sgyoung96@naver.com>
2023-06-15 09:25:14 +0900

    import 수정

--- project/back/petmily/src/main/java/com/example/demo/adoptcomment/Adoptcomment.java
+++ project/back/petmily/src/main/java/com/example/demo/adoptcomment/Adoptcomment.java
@@ -4,13 +4,13 @@ import java.util.Date;
 
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
-import org.springframework.data.annotation.Id;
 
 import com.example.demo.member.Member;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.PrePersist;
