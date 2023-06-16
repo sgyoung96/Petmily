1aeef356bb3025335371db5708d14dc71b005d88
Blaire shon <sonsy09@naver.com>
2023-06-16 11:02:07 +0900

    Member Dto, Controller 수정(jwt 추가)

--- project/back/petmily/src/main/java/com/example/demo/member/MemberController.java
+++ project/back/petmily/src/main/java/com/example/demo/member/MemberController.java
@@ -1,17 +1,21 @@
 package com.example.demo.member;
 
 import java.util.HashMap;
-
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.auth.JwtTokenProvider;
+
 
 
 @RestController
@@ -21,8 +25,8 @@ public class MemberController {
 	@Autowired
 	private MemberService service;
 	
-//	@Autowired
-//	private JwtTokenProvider tokenprovider;// 멤버변수로 토큰 브로바이더 추가
+	@Autowired
+	private JwtTokenProvider tokenprovider;// 멤버변수로 토큰 브로바이더 추가
 	
 
 	// 가입
@@ -34,17 +38,112 @@ public class MemberController {
 		return map;
 	}
 	
+	//==================== token ======================
+	
+	
 	// idcheck 검색
+	// 검색
 	@GetMapping("/{id}")
-	public Map get(@PathVariable("id") String id) {
+	public Map get(@PathVariable("id") String id, @RequestHeader(name = "token", required = false) String token) {
 
 		Map map = new HashMap();
+
+		if (token != null) { // 로그인 후
+			try {
+				String id2 = tokenprovider.getUsernameFromToken(token);
+				if (!id.equals(id2)) {
+					map.put("dto", null);
+					return map;
+				}
+			} catch (Exception e) {
+				map.put("dto", null);
+				return map;
+			}
+		}
 		MemberDto d = service.getMember(id);
 		map.put("dto", d);
 
 		return map;
+		}
+	
+	
+	
+	//로그인
+	@PostMapping("/login")
+	public Map login(String id, String pwd) {
+		Map map = new HashMap();
+		boolean flag = false;
+		MemberDto dto = service.getMember(id);
+		if (dto != null && pwd.equals(dto.getPwd())) {
+			String token = tokenprovider.generateJwtToken(dto); // 토큰 생성
+			flag = true;
+			map.put("token", token);
+		}
+		map.put("flag", flag);
+		return map;
 	}
 	
+	//검색
+	@PostMapping("/token")
+	public Map getByToken(String token) {
+		boolean flag = true;
+		Map map = new HashMap();
+		try {
+			// 받은 토큰에서 로그인한 사람의 아이디, 타입 정보를 추출
+			String id = tokenprovider.getUsernameFromToken(token);
+			map.put("id", id);
+		} catch (Exception e) {
+			flag = false;
+		}
+		map.put("flag", flag);
+		return map;
+	}
+	
+	// 검색
+	@GetMapping("")
+	public Map getInfo(@RequestHeader("token") String token) { // @RequestHeader("token") : 요청 헤더 객체 받아오기
+		boolean flag = true;
+		Map map = new HashMap();
+		try {
+			String id = tokenprovider.getUsernameFromToken(token);
+			map.put("id", id);
+		} catch (Exception e) {
+			flag = false;
+		}
+		map.put("flag", flag);
+		return map;
+		}
+	
+	// ======================================================
+	
+	//내정보 수정
+	@PutMapping("")
+	public Map edit(MemberDto dto) {
+		MemberDto old = service.getMember(dto.getId());
+		old.setEmail(dto.getEmail());
+		old.setPwd(dto.getPwd());
+		old.setPhone(dto.getPhone());
+		
+		MemberDto d = service.save(old);
+		Map map = new HashMap();
+		map.put("dto",d);
+		return map;
+	}
+	
+	//탈퇴
+	@DeleteMapping("/{id}")
+	public Map del(@PathVariable("id") String id) {
+
+		boolean flag = true;
+		try {
+			service.delMember(id);
+		} catch (Exception e) {
+			flag = false;
+		}
+		Map map = new HashMap();
+		map.put("flag", flag);
+		return map;
+	}
 	
 
 }
--- project/back/petmily/src/main/java/com/example/demo/member/MemberDto.java
+++ project/back/petmily/src/main/java/com/example/demo/member/MemberDto.java
@@ -2,6 +2,7 @@ package com.example.demo.member;
 
 import java.util.Date;
 
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,6 +16,7 @@ import lombok.ToString;
 @AllArgsConstructor
 public class MemberDto {
 	
+	
 	private String id;
 	private String token;
 	private String pwd;
@@ -25,4 +27,19 @@ public class MemberDto {
 	private String phone;
 	private String address;
 
+	@Override
+	public boolean equals(Object obj) { // equals는 object를 상속받았는데 object는 참조값이 같으면 같다
+		// TODO Auto-generated method stub
+		if(obj!=null && obj instanceof MemberDto) { // 재정의했다. 꼭 필요한건 아니지만 테스팅하려고 재정의함. 
+			MemberDto d = (MemberDto)obj;
+			if(d.id.equals(this.id)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+
+	
+	
 }
