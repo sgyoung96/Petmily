34830fbccfa567de78c2e79105997b7774e4b998
sgyoung96 <sgyoung96@naver.com>
2023-06-16 09:36:50 +0900

    service 어노테이션 추가

--- project/back/petmily/src/main/java/com/example/demo/adoptcomment/AdoptCommentService.java
+++ project/back/petmily/src/main/java/com/example/demo/adoptcomment/AdoptCommentService.java
@@ -3,7 +3,9 @@ package com.example.demo.adoptcomment;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AdoptCommentService {
 	
 	@Autowired
