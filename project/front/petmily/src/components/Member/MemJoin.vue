ac3977670e3dff94ee85b188d7d67e56b4a5213d
Blaire shon <sonsy09@naver.com>
2023-06-16 12:28:36 +0900

    0616 vue 커밋

---
+++ project/front/petmily/src/components/Member/MemJoin.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="myjoin">
+        <h3>join form</h3>
+        id:<input type="text" v-model="id"><button v-on:click="idcheck">중복체크</button><br/>
+        {{msg}}<br/>
+        pwd:<input type="password" v-model="pwd"><br/>
+        name:<input type="text" v-model="name"><br/>
+        email:<input type="text" v-model="email"><br/>
+        gender:<input name="g" type="radio" v-model="gender" value="1">남 / 
+            <input name="g" type="radio" v-model="gender" value="2">여 <br/>
+        phone:<input type="text" v-model="phone"><br/>
+        address:<input type="text" v-model="address"><br/>
+        <button v-on:click="join">가입</button>
+    </div>
+  </template>
+ 
