bd0476ed45ac4eb700d005348eded011457ccc4d
sgyoung96 <sgyoung96@naver.com>
2023-06-16 09:45:00 +0900

    DAO 에 어노테이션 추가

--- project/back/petmily/src/main/java/com/example/demo/adoptcomment/AdoptCommentDao.java
+++ project/back/petmily/src/main/java/com/example/demo/adoptcomment/AdoptCommentDao.java
@@ -3,7 +3,9 @@ package com.example.demo.adoptcomment;
 import java.util.ArrayList;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdoptCommentDao extends JpaRepository<Adoptcomment, Integer>{
 		
 	ArrayList<Adoptcomment> getAll(int num); 				// 해당 게시글 전체 댓글 조회, num : 게시글 고유 번호
