cb74af286612f4107fdf009cbe4874c4db64a349
DESKTOP-5BU0LR5\KOSTA <soundshow@naver.com>
2023-06-16 12:27:06 +0900

    입양게시판초안

--- project/front/petmily/src/router/index.js
+++ project/front/petmily/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import DiaryboardHome from '@/components/dboard/DiaryboardHome.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/diaryboardhome',
+    name: 'DiaryboardHome',
+    component: DiaryboardHome
   },
   {
     path: '/about',
