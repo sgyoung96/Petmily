332eb10dff6d05428f38826a8ae74405d34a0118
reacting97 <wnsgk828@gmail.com>
2023-06-19 16:10:01 +0900

    참가자 명수 출력

--- project/back/petmily/src/main/java/com/example/demo/participants/ParticipantsController.java
+++ project/back/petmily/src/main/java/com/example/demo/participants/ParticipantsController.java
@@ -1,9 +1,13 @@
 package com.example.demo.participants;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +18,13 @@ public class ParticipantsController {
 	@Autowired
 	private ParticipantsService service;
 	
-	@GetMapping("")
-	public Map getAll
+	@GetMapping("/num/{num}")
+	public Map getCount(@PathVariable("num") int num) {
+		long c = service.printPerson(num);
+		Map map = new HashMap();
+		map.put("count", c);
+		return map;
+	}
 	
 	
 }
