354d7f878905a0d56cff25461955f9f1872d3bcf
LAPTOP-G87K7OJC\user <johnchoi06888@gmail.com>
2023-06-20 16:46:25 +0900

    6월 20일 마무리 안해

--- project/front/petmily/src/components/cdh/Api.vue
+++ project/front/petmily/src/components/cdh/Api.vue
@@ -1,40 +1,62 @@
 <template>
-    <div id="app">
-  <table border="1px">
-    <thead>
-      <tr>
-        <th>filename</th>
-        <th>Care Name</th>
-        <th>Happen Place</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="item in items.response.body.items.item" v-bind:key="item.careNm">
-        <td><img :src="item.filename._text" :alt="item.careNm._text" /></td>
-        <td>{{ item.careNm._text }}</td>
-        <td>{{ item.happenPlace._text }}</td>
-      </tr>
-    </tbody>
-  </table>
-</div>
+  <div id="app">
+    
+    <table border="1px">
+      <thead>
+        <tr>
+          <th>filename</th>
+          <th>Care Name</th>
+          <th>Happen Place</th>
+          <th>desertionNo</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in filteredItems" :key="item.careNm">
+          <td>
+            <img :src="item.filename" :alt="item.careNm" @click="goToDetail(item.desertionNo)" style="cursor: pointer;" />
+          </td>
+          <td>{{ item.careNm }}</td>
+          <td>{{ item.happenPlace }}</td>
+          <td>{{ item.desertionNo }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-var convert = require('xml-js')
+import axios from 'axios';
+
 export default {
   name: 'App',
-  data () {
+  data() {
     return {
-      items: []
-    }
+      items: [],
+      search: '',
+    };
   },
-  created () {
-    this.$http.get('http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?pageNo=1&numOfRows=10&serviceKey=JkjPRne8oXZTCJTyLN9579FQZI6%2FkhepY9kJhsmdEpdiEjyDUj8HjiEo8ba4BAa8AOGXfQWZA7AAHiljNzoOBA%3D%3D')
+  computed: {
+    filteredItems() {
+      if (!this.search) {
+        return this.items;
+      }
+      const searchValue = this.search.toLowerCase();
+      return this.items.filter(item => item.desertionNo.toLowerCase().includes(searchValue));
+    },
+  },
+  created() {
+    axios
+      .get('http://apis.data.go.kr/1543061/abandonmentPublicSrvc/abandonmentPublic?_type=json&pageNo=1&numOfRows=24&serviceKey=JkjPRne8oXZTCJTyLN9579FQZI6%2FkhepY9kJhsmdEpdiEjyDUj8HjiEo8ba4BAa8AOGXfQWZA7AAHiljNzoOBA%3D%3D')
       .then((response) => {
-        var xml = response.data
-        var json = convert.xml2json(xml, { compact: true })
-        this.items = JSON.parse(json)
-      })
-  }
-}
+        const data = response.data;
+        const items = data.response.body.items.item;
+        this.items = items; // 데이터를 items 배열에 할당
+      });
+  },
+  methods: {
+    goToDetail(desertionNo) {
+      this.$router.push({ name: 'Detail', params: { desertionNo: desertionNo } });
+    },
+  },
+};
 </script>
