b28659d74c4916c3326ae4fbfdd2850431f52033
DESKTOP-5BU0LR5\KOSTA <soundshow@naver.com>
2023-06-30 17:34:21 +0900

    프론트업데이트

--- project/front/petmily/src/components/admin/VolList.vue
+++ project/front/petmily/src/components/admin/VolList.vue
@@ -1,32 +1,190 @@
 <template>
-  <div>
-    
-    
+  <div style="padding-left: 150px;padding-right:150px">
+    <div style="display: flex;justify-content: space-between;">
+      <div>
+        <router-link to="/volboardadd">봉사게시판 작성</router-link>
+      </div>
+      <div>
+        <input type="text" v-model="searchKeyword" placeholder="주소를 입력해주세요">
+        <button v-on:click="search()">검색</button>
+      </div>
+    </div>
+    <div style="border-top:2px solid black; padding-top:10px; margin-top:10px">
+      
+      <div class="vbody" v-for="vboard in list" :key="vboard.num" @click="detail(vboard.num, vboard.address)">
+        <div style="text-align: left;">
+            <span class="badge text-bg-danger" style="font-size: 17px;" v-if="calculateDateDifference(vboard.deadline).difference < 0">모집마감</span>
+            <span class="badge text-bg-primary" style="font-size: 17px;" v-else >마감 D-{{ calculateDateDifference(vboard.deadline).days }}</span>&nbsp;
+          <strong>모집기간</strong> {{ formatDate(vboard.deadline) }}까지
+          <br />
+          <div class=vtitle>{{ vboard.title }}</div>
+          <div style="margin-bottom: 10px;">
+            <strong>[모집기관]</strong> {{ vboard.place }} &nbsp;
+            <strong>[봉사날짜]</strong> {{ formatDate(vboard.vol_date) }} &nbsp;<br />
+            <strong>[봉사장소]</strong> {{ vboard.address }}
+          </div>
+        </div>
+        <div style class="vcount">
+          모집인원<br />({{ vboard.count }} / {{ vboard.vol_number }})
+        </div>
+      </div>
+
+    </div>
   </div>
+  <ul class="pagination" style="display: inline-block">
+    <!-- 이전 페이지 버튼 -->
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous" @click="previousPage">
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+    <!-- 페이지 번호 -->
+    <li class="page-item" v-for="pageNumber in totalPages" :key="pageNumber"
+      :class="{ active: pageNumber === currentPage }">
+      <a class="page-link" href="#" @click="goToPage(pageNumber)">{{ pageNumber }}</a>
+    </li>
+    <!-- 다음 페이지 버튼 -->
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Next" @click="nextPage">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
+  </ul>
 </template>
 
 <script>
-
 export default {
   name: 'VolList',
-  data () {
+  data() {
     return {
-        
+      loginId: sessionStorage.getItem('loginId'),
+      list: [],
+      count: 0,
+      state: '',
+      sysdate: new Date(),
+      currentPage: 1,
+      pageSize: 8,
+      searchKeyword: ''
+    }
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.list.length / this.pageSize);
+    },
+    paginatedList() {
+      const startIndex = (this.currentPage - 1) * this.pageSize;
+      const endIndex = startIndex + this.pageSize;
+      return this.list.slice(startIndex, endIndex);
+    }
+  },
+  methods: {
+    search(){
+      const self = this
+      let address = self.searchKeyword
+      self.$axios.get('http://localhost:8082/volboard/address/' + address)
+      .then(function (response){
+        if(response.status == 200){
+          self.list = response.data.list
+        }else{
+          alert('에러')
+        }
+      })
+    },
+    detail(num, address) {
+      this.$router.push({
+        name: 'VolBoardDetail',
+        query: { num: num, address: address },
+      })
+    },
+    calculateDateDifference(deadline) {
+      const deadlineDate = new Date(deadline)
+      const difference = deadlineDate.getTime() - this.sysdate.getTime()
+      const days = Math.ceil(difference / (1000 * 60 * 60 * 24))
+      return { difference, days }
+    },
+    formatDate(date) {
+      const options = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+      };
+
+      return new Date(date).toLocaleString('ko-KR', options).replace(/\D/g, '');
+    },
+    previousPage() {
+      if (this.currentPage > 1) {
+        this.currentPage--;
+      }
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) {
+        this.currentPage++;
+      }
+    },
+    goToPage(pageNumber) {
+      this.currentPage = pageNumber;
     }
   },
   created: function () {
-    
+    this.loginId = sessionStorage.getItem('loginId')
+    const self = this
+    self.$axios.get('http://localhost:8082/volboard').then(function (res) {
+      if (res.status == 200) {
+        self.list = res.data.list
+      } else {
+        alert('에러')
+      }
+    })
   },
-  methods: {
-    
-
-  },
-  components: {
-    
-  }
 }
 </script>
-  <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
 
+<style scoped>
+.v-title {
+  margin-top: 120px;
+  margin-bottom: 120px;
+}
+
+.vbody {
+  border-bottom: 1px solid #666666;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 10px;
+  cursor: pointer;
+}
+
+.vcount {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 70px;
+  height: 70px;
+  border: 2px solid #666666;
+  font-size: 15px;
+  font-weight: bold;
+  text-align: center;
+  margin-top: 20px;
+}
+
+.vtitle {
+  font-size: 30px;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
 </style>
