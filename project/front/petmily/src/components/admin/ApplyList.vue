cc346b76dc59334cb65da2eee7b7c971e0126bd6
sgyoung96 <sgyoung96@naver.com>
2023-06-30 17:33:23 +0900

    버그수정

--- project/front/petmily/src/components/admin/ApplyList.vue
+++ project/front/petmily/src/components/admin/ApplyList.vue
@@ -1,30 +1,57 @@
 <template>
   <div>
-    <h3>입양 신청 목록</h3>
-    <table border="1">
-      <tr>
-        <th>num</th>
-        <th>id</th>
-        <th>wdate</th>
-        <th>reason</th>
-        <th>feeding</th>
-        <th>petCd</th>
-      </tr>
-      <tr v-for="(apply, index) in list" :key="index">
-        <td>{{apply.num}}</td>
-        <td>{{ apply.id.id }}</td>
-        <td>{{ apply.wdate }}</td>
-        <td>{{ apply.reason }}</td>
-        <td>{{ apply.feeding }}</td>
-        <td>{{ apply.petCd }}</td>
-        <td>{{apply.ischeck}}</td>
-        <td>
-          <button @click="apply_form(apply.num)" style="background-color:#FFD65B; border-radius:10px;">승인</button>
-          <button @click="refuse_form(apply.num)" style="background-color:#FFD65B; border-radius:10px;">거부</button>
-        </td>
-      </tr>
-    </table>
+    <div>
 
+      <div class="form-container">
+
+        <div class="list-container">
+          <p><label class="section01" >SECTION 1</label></p>
+          <p><label class="section01-title"><u><span>간편 승인/반려 영역</span></u></label></p>
+          <div class="section01-content">
+            <div class="data-header">
+              <table>
+                <tr aria-colspan="12">
+                  <th colspan="1">
+                    <label><span>NO</span></label>
+                  </th>
+                  <th colspan="1">
+                    <label><span>ID</span></label>
+                  </th>
+                  <th colspan="1">
+                    <label><span>NAME</span></label>
+                  </th>
+                  <th colspan="5">
+                    <label><span>REASON</span></label>
+                  </th>
+                  <th colspan="2"> 
+                    <label><span>DATE</span></label>
+                  </th>
+                  <th colspan="1">
+                    <label><span>STATE</span></label>
+                  </th>
+                  <th colspan="1"> 
+                    <label><span>APPROVE</span></label> |
+                    <label><span>REFUSE</span></label>
+                  </th>
+                </tr>
+                <tr v-for="(apply, index) in list" :key="index" aria-colspan="12" @click="goDetail(index, apply.num)">
+                  <td colspan="1" :id="index"><label><span>{{ index + 1 }}</span></label></td>
+                  <td colspan="1"><label><span>{{ apply.id.id }}</span></label></td>
+                  <td colspan="1"><label><span>{{ apply.id.name }}</span></label></td>
+                  <td colspan="5" width="300px"><label><span>{{ apply.reason }}</span></label></td>
+                  <td colspan="2"><label><span>{{apply.wdate.split('T')[0]}}</span></label></td>
+                  <td colspan="1"><label><span v-if="apply.ischeck == 0">요청</span><span v-else-if="apply.ischeck == 1">승인</span><span v-else-if="apply.ischeck==2">반려</span><span v-else>오류</span></label></td>
+                  <td colspan="1">
+                    <button @click="apply_form(apply.num)" style="background-color:#FFD65B; border-radius:10px;">승인</button>
+                    <button @click="refuse_form(apply.num)" style="background-color:#FFD65B; border-radius:10px;">거부</button>
+                  </td>
+                </tr>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,7 +61,18 @@ export default {
   name: 'AdminAppliedList',
   data() {
     return {
-      list: []
+      list: [],
+      num: '',
+      id: '',
+      wdate: '',
+      agreement: '',
+      another: '',
+      reason: '',
+      feeding: '',
+      ischeck: '',
+      petCd: '',
+
+      index: '',
     }
   },
   created: function () {
@@ -42,7 +80,8 @@ export default {
     self.$axios.get('http://localhost:8082/Applyform')
       .then(function (res) {
         if (res.status == 200) {
-          self.list = res.data.list
+          self.list = res.data.list;
+          self.setData();
         } else {
           alert('에러코드' + res.status)
         }
@@ -74,6 +113,12 @@ export default {
             alert('에러코드' + res.status);
           }
         });
+    },
+    setData() { // 데이터 가공
+   
+    },
+    goDetail(rowIndex, formNum) {
+      console.log(rowIndex, formNum);
     }
   },
   components: {
@@ -82,5 +127,86 @@ export default {
 }
 </script>
   <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+
+.form-container {
+  display: block;
+  position: relative;
+}
+
+.list-container {
+  display: block;
+  position: relative;
+  margin-left: 50px;
+  margin-right: 50px;
+  padding-top: 40px;
+  padding-bottom: 100px;
+  border: 5px solid #efefef;
+  border-radius: 10px;
+  height: auto;
+}
+
+.section01 {
+  font-family: 'IBMPlexSansKR-Bold';
+  font-size: 15px;
+  text-align: center;
+  vertical-align: center;
+  width: 120px;
+  height: 25px;
+  background: rgb(244, 191, 79);
+  color: white;
+  border-radius: 10px;
+}
+
+.section01-content {
+  border: 1px solid #efefef;
+  border-radius: 20px;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+
+.section01-title u span {
+  font-family: 'IBMPlexSansKR-Bold';
+  font-size: 18px;
+  color: black;
+}
+
+.section01-title u {
+  text-decoration-color: rgb(244, 191, 79);
+  text-decoration-thickness: 3px;
+}
+
+.data-header {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 80px;
+  padding-right: 80px;
+}
+
+table {
+  width: 100%;
+}
+
+.data-header table tr th {
+  font-family: 'IBMPlexSansKR-Bold';
+  font-size: 14px;
+  color: black;
+  text-align: center;
+}
+
+.data-header table tr td {
+  font-family: 'IBMPlexSansKR-Regular';
+  font-size: 14px;
+  color: black;
+  text-align: center;
+} 
+
+.data-contents {
+  display: flex;
+  justify-content: space-between;
+  padding-left: 60px;
+  padding-right: 80px;
+}
+
+</style>
 
