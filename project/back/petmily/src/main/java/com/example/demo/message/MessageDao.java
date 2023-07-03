7121e1ebfe6f37a83bc8c26adc4ed42092e5cfc1
Blaire shon <sonsy09@naver.com>
2023-07-03 09:17:42 +0900

    쪽지, 회원가입 develop

--- project/back/petmily/src/main/java/com/example/demo/message/MessageDao.java
+++ project/back/petmily/src/main/java/com/example/demo/message/MessageDao.java
@@ -1,6 +1,7 @@
 package com.example.demo.message;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -15,10 +16,16 @@ import jakarta.transaction.Transactional;
 public interface MessageDao extends JpaRepository<Message, Integer> {
 	ArrayList<Message> findByRecieverAndAvailablereciever(Member loginId, String del); // 받은 메세지함 목록
 	ArrayList<Message> findBySenderAndAvailablesender(Member sender, String del); // 보낸이로 검색	
-	ArrayList<Message> findByRecieverAndCheck(Member loginId, int check); // 읽은 메세지, 안읽은 메세지 검색
+	ArrayList<Message> findByRecieverAndCheckAndAvailablereciever(Member loginId, int check, String del); // 내가 받은 메세지 중 읽은 메세지, 안읽은 메세지 검색(삭제메세지 제외)
+	ArrayList<Message> findBySenderAndCheckAndAvailablesender(Member loginId, int check, String del); // 내가 보낸 메세지 중 수신자가 읽은 메세지, 안읽은 메세지 검색(삭제메세지 제외)
+
 	
 	
+	@Query("SELECT m FROM Message m WHERE m.reciever = :reciever AND m.title LIKE %:title% AND m.availablereciever = :availablereciever") //받은 메세지 제목으로 검색
+	List<Message> findByTitleRecieve(@Param("reciever") Member reciever, @Param("title") String title, @Param("availablereciever") String availablereciever);
 	
+	@Query("SELECT m FROM Message m WHERE m.sender = :sender AND m.title LIKE %:title% AND m.availablesender = :availablesender") //보낸 메세지 제목으로 검색
+	List<Message> findByTitleSend(@Param("sender") Member sender, @Param("title") String title, @Param("availablesender") String availablesender);
 	
 	@Transactional
 	@Modifying
