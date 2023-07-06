6500e50918fba53c65a4d822027c2e859e77a4aa
reacting97 <wnsgk828@gmail.com>
2023-07-06 17:42:54 +0900

    충돌 해결푸시

--- project/back/petmily/src/main/java/com/example/demo/notification/NotificationDao.java
+++ project/back/petmily/src/main/java/com/example/demo/notification/NotificationDao.java
@@ -15,15 +15,9 @@ public interface NotificationDao extends JpaRepository<Notification, Integer> {
 	 * @param id
 	 * @return
 	 */
-<<<<<<< HEAD
-	@Transactional
-	@Query(value="SELECT n FROM Notification n JOIN n.reciever r WHERE r = :reciever AND n.isClicked = '0' ORDER BY n.trDate DESC")
-	ArrayList<Notification> getNew2Notification(@Param("reciever") Member reciever);
 
-=======
 	@Query(value = "SELECT num, reciever, header, content, row_num, title, writer, commenter, notify_type, is_clicked, tr_date FROM Notification WHERE reciever = :reciever AND is_clicked = '0' ORDER BY tr_date DESC")
 	ArrayList<Notification> newNotification(@Param("reciever") String reciever);
->>>>>>> origin/sgy
 	
 	/**
 	 * 모든 알림 받는 사람으로 전체 검색
@@ -39,15 +33,9 @@ public interface NotificationDao extends JpaRepository<Notification, Integer> {
 	 * @param id
 	 * @return
 	 */
-<<<<<<< HEAD
-	@Transactional
-	@Query(value="SELECT n FROM Notification n JOIN n.receiver r JOIN n.board b WHERE r = :receiver AND b INSTANCE OF AdoptBoard AND n.notifyType = '1' ORDER BY n.trDate DESC")
-	ArrayList<Notification> getAdoptComment2Notification(@Param("receiver") Member receiver);
 
-=======
 	@Query(value = "SELECT num, reciever, header, content, row_num, title, writer, commenter, notify_type, is_clicked, tr_date FROM Notification WHERE reciever = :reciever AND notify_type = '1' ORDER BY tr_date DESC")
 	ArrayList<Notification> adoptNotification(@Param("reciever") String reciever);
->>>>>>> origin/sgy
 	
 	/**
 	 * 입양일지 게시판 댓글 알림
